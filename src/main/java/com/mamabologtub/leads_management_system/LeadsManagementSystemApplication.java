cc048a656c2bd34cc2d7919f31653a5901beeb73
MamaboloGtub <tmmmmadi@gmail.com>
2025-10-18 13:31:58 +0200

    scaffolds

---
+++ src/main/java/com/mamabologtub/leads_management_system/LeadsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mamabologtub.leads_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mamabologtub/leads_management_system/LeadsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamabologtub.leads_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
